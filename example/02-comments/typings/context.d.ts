c57d5aab158b574d30e88251da876868a4429a1b
xiaojue <designsor@gmail.com>
2019-04-28 22:26:16 +0800

    feat: add Request,Response interface on Daruk

--- example/02-comments/typings/context.d.ts
+++ example/02-comments/typings/context.d.ts
@@ -1,5 +1,12 @@
+import { Request } from 'daruk';
+
+interface IRequest extends Request {
+  body: any;
+}
+
 declare module 'daruk' {
   interface Context {
     render: (tpl: string, data?: any) => Promise<any>;
+    request: IRequest;
   }
 }
