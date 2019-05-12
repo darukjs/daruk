78b56a2bba6adbd875f6edb4344a22bb78c34822
himself65 <himself65@outlook.com>
2019-05-12 12:46:48 +0800

    fix: windows test run error

--- src/core/daruk_init_module.ts
+++ src/core/daruk_init_module.ts
@@ -186,9 +186,8 @@ export default class DarukInitModule {
           Reflect.getMetadata(CONTROLLER_REDIRECT_PATH, ControllerClass, funcName) || '';
         // 避免解析出的路由没有 / 前缀
         // 并保证前后都有 /，方便后续比对路由 key
-        let routePath = ujoin('/', prefixPath);
         // 不转path，因为可能会把通配符转成unix path
-        routePath = join('/', prefix, routePath, path, '/');
+        const routePath = ujoin('/', prefix, '/', prefixPath, path, '/');
         // 将路由按照 http method 分组
         routeMap[method] = routeMap[method] || [];
         // 判断路由是否重复定义
