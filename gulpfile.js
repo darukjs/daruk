7e4304d07b6fafd57f7151e1c124d22e635899f0
xiaojue <designsor@gmail.com>
2019-05-12 18:12:53 +0800

    test: update example dev model

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const gulp = require('gulp');
-const rimraf = require('rimraf')
+const rimraf = require('rimraf');
 const exec = require('child_process').exec;
 
 gulp.task('move:build', () => moveDirToExample('build/**/*', '/node_modules/daruk'));
@@ -10,40 +10,33 @@ gulp.task('move:types', () => moveDirToExample('types/**/*', '/node_modules/@typ
 
 gulp.task('move', gulp.parallel('move:build', 'move:types'));
 
-gulp.task('build', cb => exec('npm run build', cb))
+gulp.task('build', (cb) => exec('npm run build', cb));
 
 gulp.task('watch:src', () => {
   const watcher = gulp.watch('src/**/*.ts');
-  watcher.on('all',
-    gulp.series('build', 'move:build')
-  )
-})
+  watcher.on('all', gulp.series('build', 'move:build', 'move:types'));
+});
 
 gulp.task('watch:types', () => {
   const watcher = gulp.watch('types/**/*.d.ts');
-  watcher.on('all', gulp.series('move:types'))
-})
+  watcher.on('all', gulp.series('build', 'move:build', 'move:types'));
+});
 
-gulp.task('watch', gulp.parallel('watch:src', 'watch:types'))
+gulp.task('watch', gulp.parallel('watch:src', 'watch:types'));
 
-gulp.task('build:watch', gulp.series('move', 'watch'))
-
+gulp.task('build:watch', gulp.series('move', 'watch'));
 
 function moveDirToExample(src, dest) {
-  const getDirs = base => getFolders(base).map(path => `${base}/${path}`);
+  const getDirs = (base) => getFolders(base).map((path) => `${base}/${path}`);
   const examplesDirs = getDirs('example');
   let stream = gulp.src([src]);
-  examplesDirs.forEach(dir => {
-    rimraf.sync(dir + dest)
-    stream = stream
-      .pipe(gulp
-        .dest(dir + dest));
+  examplesDirs.forEach((dir) => {
+    rimraf.sync(dir + dest);
+    stream = stream.pipe(gulp.dest(dir + dest));
   });
   return stream;
 }
 
 function getFolders(dir) {
-  return fs.readdirSync(dir).filter(file =>
-    fs.statSync(path.join(dir, file)).isDirectory()
-  );
+  return fs.readdirSync(dir).filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
 }
