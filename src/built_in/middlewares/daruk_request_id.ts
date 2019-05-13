f85ade6b666aa704b64b4540b44d022bca9c1cb3
icepy <xiangwenwe@foxmail.com>
2019-05-14 00:23:41 +0800

    fix: Import sources within a group must be alphabetized.

--- src/built_in/middlewares/daruk_request_id.ts
+++ src/built_in/middlewares/daruk_request_id.ts
@@ -2,8 +2,8 @@
  * @fileOverview request id 中间件
  */
 
-import { Daruk } from '../../typings/daruk';
 import { xRequestId } from 'daruk-request-id';
+import { Daruk } from '../../typings/daruk';
 
 export default (app: Daruk.DarukCore) => {
   return xRequestId(app.options.requestId, app);
