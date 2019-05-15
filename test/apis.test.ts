f401e21947c0372c3e5751ee76c5756a088559e8
xiaojue <designsor@gmail.com>
2019-05-15 19:32:22 +0800

    test: add listen and run test

--- test/apis.test.ts
+++ test/apis.test.ts
@@ -46,11 +46,59 @@ describe('apis', () => {
     assert(context.host === host);
   });
 
+  it('listen args with port, host, cb', (done) => {
+    let port = 3000;
+    app.listen(port, '127.0.0.1', () => {
+      app.httpServer.close(done);
+    });
+  });
+
+  it('run args with port, cb', (done) => {
+    let port = 3000;
+    app.listen(port, () => {
+      app.httpServer.close(done);
+    });
+  });
+
+  it('run args with object, cb', (done) => {
+    let port = 3000;
+    app.listen(
+      {
+        port
+      },
+      () => {
+        app.httpServer.close(done);
+      }
+    );
+  });
+
+  it('run args with object', (done) => {
+    let port = 3000;
+    app.listen({
+      port
+    });
+    setTimeout(() => {
+      app.httpServer.close(done);
+    }, 1000);
+  });
+
+  it('run args with port, host, cb', (done) => {
+    let port = 3000;
+    app.run(port, '127.0.0.1', () => {
+      app.httpServer.close(done);
+    });
+  });
+
+  it('run args with port, cb', (done) => {
+    let port = 3000;
+    app.run(port, () => {
+      app.httpServer.close(done);
+    });
+  });
+
   it('should log error when use change modules', () => {
     assert.throws(() => {
-      app.util.testUtil = function changedModule() {
-
-      };
-    }, '[daruk error] user could not change module directly\nplease use function\'setModule\'');
+      app.util.testUtil = function changedModule() {};
+    }, "[daruk error] user could not change module directly\nplease use function'setModule'");
   });
 });
