9b2d7b48e8cebf724b8c511cada83839bb0cb234
xiaojue <designsor@gmail.com>
2020-01-19 20:21:22 +0800

    feat:add @injectable,@provide,darukContainer,server,@inject,interface,Daruk,TYPES export

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
+import { injectable, interfaces } from 'inversify';
+import { provide } from 'inversify-binding-decorators';
 // tslint:disable-next-line
 import 'reflect-metadata';
 import Daruk from './core/daruk';
-import { darukContainer, lazyInject } from './core/inversify.config';
+import { darukContainer, lazyInject as inject } from './core/inversify.config';
 import { TYPES } from './core/types';
 export * from './decorators';
-
 const server = darukContainer.get<Daruk>(TYPES.Daruk);
-
-(async () => {
-  server.initOptions({
-    middwareOrder: ['daruk_logger', 'daruk_request_id', 'daruk_body']
-  });
-  await server.initPlugin();
-  // tslint:disable-next-line
-  server.listen(3000);
-})();
+export { server, darukContainer, inject, provide, injectable, interfaces, Daruk, TYPES };
