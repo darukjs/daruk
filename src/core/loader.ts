1da29faa0e06b49ede4329bdecce1a231d296ef2
xiaojue <designsor@gmail.com>
2020-01-19 17:21:36 +0800

    refactor: remove loadDarukConfigMid

--- src/core/loader.ts
+++ src/core/loader.ts
@@ -21,44 +21,4 @@ export default class Loader {
         uRequire(file);
       });
   }
-  /**
-   * @desc 加载 daurk.config 配置的中间件
-   */
-  public loadDarukConfigMid(midConfig: any) {
-    const middleware: any = {};
-    const globalMiddleware: any = {};
-    Object.keys(midConfig).forEach((key: string) => {
-      const config = midConfig[key];
-      let midName = key;
-      let midExport;
-      let packetName;
-      // 如果中间件的配置是一个对象
-      if (is.object(config)) {
-        packetName = config.packet;
-        midExport = config.export;
-      } else {
-        // 否则，值为一个函数，函数返回 middleware 内容
-        packetName = key;
-        midExport = config;
-      }
-      let packet;
-      try {
-        packet = require(packetName);
-        // 有些包导出 export default function
-        if (typeof packet !== 'function' && typeof packet.default === 'function') {
-          packet = packet.default;
-        }
-      } catch (e) {
-        throw new Error(`[daruk.config.middleware] require ${packetName} failed -  ${e.message}`);
-      }
-      assert(isFn(packet), `[daruk.config.middleware] can not find function at packet: ${packet}.`);
-      assert(isFn(midExport), `[daruk.config.middleware] ${key} must be (or export) a function`);
-      middleware[midName] = midExport(packet);
-      globalMiddleware[midName] = packet;
-    });
-    return {
-      middleware,
-      globalMiddleware
-    };
-  }
 }
