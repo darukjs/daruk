6586bc4fed1eb70ab2852cb4134de380357e4ae2
xiaojue <designsor@gmail.com>
2020-03-10 17:34:26 +0800

    docs: update example 04-timers

--- example/04-timers/index.ts
+++ example/04-timers/index.ts
@@ -1,6 +1,12 @@
-import { Daruk } from 'daruk';
+import { DarukServer } from '../../src';
 
-const myApp = new Daruk('myapp', { rootPath: __dirname, debug: process.env.NODE_ENV === 'dev' });
-const port = 1318;
+(async () => {
+  let app = DarukServer();
+  app.initOptions({
+    rootPath: __dirname,
+    debug: process.env.NODE_ENV === 'dev'
+  });
 
-myApp.run(port);
+  await app.loadFile('./timers');
+  await app.initPlugin();
+})();
--- example/04-timers/timers/MemInfo.ts
+++ example/04-timers/timers/MemInfo.ts
@@ -1,28 +1,31 @@
-import { Daruk } from 'daruk';
 import { freemem } from 'os';
+import { CronJob, Daruk, injectable, timer, TimerClass } from '../../../src';
 
-export default function(daruk: Daruk) {
-  return {
-    cronTime: '* * * * * *',
-    onTick(this: any) {
-      // if you setup with pm2 cluster:
-      // https://pm2.io/doc/en/runtime/guide/load-balancing/?utm_source=pm2&utm_medium=website&utm_campaign=rebranding#cluster-environment-variable
-      // only run tick on NODE_APP_INSTANCE === '0';
-      if (!process.env.NODE_APP_INSTANCE || process.env.NODE_APP_INSTANCE === '0') {
-        const unit = 1024;
-        let freeMem = Math.round(freemem() / (unit * unit));
-        daruk.logger.info({
-          freeMem
-        });
-      }
-    },
-    onComplete(this: any) {
-      // if you setup with pm2 cluster:
-      // https://pm2.io/doc/en/runtime/guide/load-balancing/?utm_source=pm2&utm_medium=website&utm_campaign=rebranding#cluster-environment-variable
-      // use the NODE_APP_INSTANCE to stop the task
-      if (!process.env.NODE_APP_INSTANCE && process.env.NODE_APP_INSTANCE !== '0') {
-        this.stop();
-      }
+@injectable()
+@timer()
+class MyTimer implements TimerClass {
+  public cronTime: string;
+  public initTimer(daruk: Daruk) {
+    this.cronTime = '* * * * * *';
+  }
+  public onTick(job: CronJob, daruk: Daruk) {
+    // if you setup with pm2 cluster:
+    // https://pm2.io/doc/en/runtime/guide/load-balancing/?utm_source=pm2&utm_medium=website&utm_campaign=rebranding#cluster-environment-variable
+    // only run tick on NODE_APP_INSTANCE === '0';
+    if (!process.env.NODE_APP_INSTANCE || process.env.NODE_APP_INSTANCE === '0') {
+      const unit = 1024;
+      let freeMem = Math.round(freemem() / (unit * unit));
+      daruk.logger.info({
+        freeMem
+      });
     }
-  };
+  }
+  public onComplete(job: CronJob, daruk: Daruk) {
+    // if you setup with pm2 cluster:
+    // https://pm2.io/doc/en/runtime/guide/load-balancing/?utm_source=pm2&utm_medium=website&utm_campaign=rebranding#cluster-environment-variable
+    // use the NODE_APP_INSTANCE to stop the task
+    if (!process.env.NODE_APP_INSTANCE && process.env.NODE_APP_INSTANCE !== '0') {
+      job.stop();
+    }
+  }
 }
