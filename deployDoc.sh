cd6a49d8d90e6228eba6943bb8c52dc4852a1e17
xiaojue <designsor@gmail.com>
2020-09-04 15:41:27 +0800

    doc: add github page deploy sh

---
+++ deployDoc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run doc
+
+# navigate into the build output directory
+cp CNAME docs/
+cd docs/
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy doc'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f https://github.com/Youjingyu/clinic-doc-zh.git master:gh-pages
+git push -f git@github.com:darukjs/daruk.git master:gh-pages
+
+cd -
