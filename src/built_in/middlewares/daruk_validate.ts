d2587b76e5c3673836bb2bfbecf7718390e75e9d
zuigzm <zuigzm@gmail.com>
2021-05-19 23:11:09 +0800

    添加validate

--- src/built_in/middlewares/daruk_validate.ts
+++ src/built_in/middlewares/daruk_validate.ts
@@ -1,12 +1,11 @@
-
 import Daruk from '../../core/daruk';
 import { defineMiddleware } from '../../decorators';
 import { MiddlewareClass } from '../../typings/daruk';
-import { parameter } from "daruk-validate";
+import { parameter } from 'daruk-validate';
 
-@defineMiddleware("daruk_validate")
+@defineMiddleware('daruk_validate')
 class DarukValidate implements MiddlewareClass {
   public initMiddleware(daruk: Daruk) {
-    parameter(daruk.app as any);
+    parameter(daruk.app, daruk.options.validateOptions);
   }
 }
