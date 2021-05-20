45c03d88ccb1940eb786f22412abf1b8fe85f1e9
zuigzm <zuigzm@gmail.com>
2021-05-20 20:09:33 +0800

    add validate test

--- test/decorators.test.ts
+++ test/decorators.test.ts
@@ -158,76 +158,7 @@ describe('decorators', () => {
       .expect('routeMiddleware multiRouteMiddleware', done);
   });
 
-  it('decorator "@required" success', (done) => {
-    request(app)
-      .post('/required/1')
-      .send({ foo: 2 })
-      .query({ bar: 3 })
-      .expect(code200)
-      .expect('', done);
-  });
-  it('decorator "@required" fail', (done) => {
-    request(app).post('/required/1').expect(code200).expect(
-      {
-        part: 'body',
-        key: 'foo'
-      },
-      done
-    );
-  });
-  it('decorator @typeParse', (done) => {
-    request(app)
-      .post('/typeparse/123')
-      .send({ foo: '123', arr: '[1]', object2: '{"a":1}', arr2: '[1,2,3]' })
-      .query({ bar: [1, 2, 3], object2: '{a:1}', arr2: '1,2,3,4' })
-      .expect(code200)
-      .expect(
-        {
-          body: {
-            foo: true,
-            arr: [1],
-            arr2: [1, 2, 3],
-            object2: { a: 1 }
-          },
-          params: { id: 123 },
-          query: { bar: '1,2,3', object2: {}, arr2: ['1,2,3,4'] }
-        },
-        done
-      );
-  });
-
-  it('decorator @typeParse with x-www-form-urlencoded', (done) => {
-    request(app)
-      .post('/typeparse/123')
-      .send('arr=[1]')
-      .expect(code200)
-      .expect(
-        {
-          body: {
-            arr: [1]
-          },
-          params: { id: 123 },
-          query: {}
-        },
-        done
-      );
-  });
-
-  it('decorator @typeParse without request args', (done) => {
-    request(app)
-      .post('/typeparse/123')
-      .expect(code200)
-      .expect(
-        {
-          body: {},
-          params: { id: 123 },
-          query: {}
-        },
-        done
-      );
-  });
-
-  it('decorator @validate success', (done) => {
+  it('decorator @validate get success', (done) => {
     request(app)
       .get('/validate')
       .query({
@@ -236,14 +167,17 @@ describe('decorators', () => {
       .expect(code200, done);
   });
 
-  it('decorator @validate fail', (done) => {
+  it('decorator @validate post success', (done) => {
     request(app)
       .post('/validate')
       .send({
         foo: 'foo'
       })
-      .expect(code200)
-      .expect('foo not pass validate!', done);
+      .expect(code200, done);
+  });
+
+  it('decorator @validate post default success', (done) => {
+    request(app).post('/validate').expect(code200, done);
   });
 
   it('decorator @cache', (done) => {
