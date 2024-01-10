9c718547e00561f5ba891ea9339983d8b07a64b5
xiaojue <designsor@gmail.com>
2024-01-10 16:39:45 +0800

    fix:fix tslint

--- src/typings/daruk_options.d.ts
+++ src/typings/daruk_options.d.ts
@@ -25,11 +25,11 @@ export interface Options {
   middlewareOrder: string[];
   name: string;
   rootPath: string;
-	routerOptions:any;
+  routerOptions: any;
   debug: boolean;
   bodyOptions: koaBody.IKoaBodyOptions;
   exitHook: boolean;
-	swagger:any;
+  swagger: any;
   // monitor: {
   //   enable: boolean;
   //   v8AnalyticsPath: string;
